0fdbbe98f7dc8c6d85cfe8600f0d942902617e67
zeshanwd <zeshan9choudhary@gmail.com>
2019-02-12 20:29:01 +0100

    added timestamps to messages

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+// var date = new Date();
+
+// console.log(date.getMonth())
+
+const date = moment();
+
+console.log(date.format('MMM Do, YYYY h:mm a'))
--- public/js/index.js
+++ public/js/index.js
@@ -9,10 +9,11 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (newMessage) {
-  console.log('new Message received from server', newMessage);
+
+  const formattedTime = moment(newMessage.createdAt).format('h:mm a');
 
   var li = jQuery('<li></li>');
-  li.text(`${newMessage.from}: ${newMessage.text}`);
+  li.text(`${newMessage.from} ${formattedTime}: ${newMessage.text}`);
 
   jQuery('#messages').append(li);
 });
@@ -22,7 +23,9 @@ socket.on('newLocationMessage', function (message) {
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My Current Location</a>');
 
-  li.text(`${message.from}: `);
+  const formattedTime = moment(message.createdAt).format('h:mm a');
+
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery("#messages").append(li);
