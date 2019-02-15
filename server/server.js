927123ac14e52d91707f733f3dc4a4bd69e0d00e
zeshanwd <zeshan9choudhary@gmail.com>
2019-02-15 21:18:23 +0100

    added people user list

--- server/server.js
+++ server/server.js
@@ -6,10 +6,13 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const { isRealString } = require('./utils/validation');
 
+const { Users } = require('./utils/users');
+
 const { generateMessage, generateLocationMessage, } = require('./utils/message');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -18,10 +21,14 @@ io.on('connection', (socket) => {
 
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
-      callback('Name and room name are required');
+      return callback('Name and room name are required');
     }
 
     socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
+
+    io.to(params.room).emit('updateUserList', users.getUserList(params.room));
 
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App'));
 
@@ -45,7 +52,12 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('User was Disconnected');
+    var user = users.removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+    }
   });
 
 });
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,6 +2,29 @@ const expect = require('expect');
 const { Users } = require('./users');
 
 describe('Users', () => {
+
+  var users;
+  beforeEach(() => {
+    users = new Users();
+    users.users = [
+      {
+        id: '1',
+        name: 'Mike',
+        room: 'Node Course',
+      },
+      {
+        id: '2',
+        name: 'Jen',
+        room: 'React Course',
+      },
+      {
+        id: '3',
+        name: 'Julie',
+        room: 'Node Course',
+      }
+    ]
+  })
+
   it('should add new user', () => {
     var users = new Users();
     var user = {
@@ -11,5 +34,39 @@ describe('Users', () => {
     }
     var res = users.addUser(user.id, user.name, user.room);
     expect(users.users).toEqual([user]);
+  })
+
+  it('should remove an user', () => {
+    var user = users.removeUser('2');
+
+    expect(users.users.length).toBe(2);
+    expect(user.id).toBe('2');
+  });
+
+  it('should not remove user', () => {
+    var user = users.removeUser('5');
+
+    expect(users.users.length).toBe(3);
+    expect(user).toNotExist();
+  });
+
+  it('should find user', () => {
+    const user = users.getUser('1');
+    expect(user.id).toBe('1');
+  })
+
+  it('should not get user', () => {
+    const user = users.getUser('1sdgfds');
+    expect(user).toNotExist();
+  });
+
+  it('should return names for node course', () => {
+    var userList = users.getUserList('Node Course');
+    expect(userList).toEqual(['Mike', 'Julie']);
+  })
+
+  it('should return names for react course', () => {
+    var userList = users.getUserList('React Course');
+    expect(userList).toEqual(['Jen']);
   })
 })
